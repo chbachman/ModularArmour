3a9941546366d209604cec4578014db7d0654754
chbachman <cbachman@bachmangroup.com>
2015-01-08 12:56:12 -0600

    Update version, load after mod dependencies

--- src/main/java/chbachman/armour/reference/Reference.java
+++ src/main/java/chbachman/armour/reference/Reference.java
@@ -6,7 +6,7 @@ public class Reference {
     public final static String MODNAME = "Modular Armour";
     public final static String VERSION = "0.1.4";
     public final static String CHANNEL = "ModularArmour";
-    public final static String DEPENDENCIES = "required-after:CoFHCore;after:Thaumcraft;after:ThermalExpansion;after:Baubles";
+    public final static String DEPENDENCIES = "required-after:CoFHCore;after:Thaumcraft;after:ThermalExpansion;after:Baubles;after:Botania;after:AWWayOfTime;after:MineTweaker3";
     
     // ;after:ThermalExpansion
     
