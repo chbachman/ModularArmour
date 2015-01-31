848068c8f5c379a56f2ee666ab57b7013e655ba9
chbachman <cbachman@bachmangroup.com>
2015-01-31 10:41:45 -0600

    Hopefully fix Blood Magic API

---
+++ src/api/java/WayofTime/alchemicalWizardry/common/spell/complex/effect/SpellHelper.java
@@ -0,0 +1,15 @@
+package WayofTime.alchemicalWizardry.common.spell.complex.effect;
+
+import net.minecraft.util.Vec3;
+import WayofTime.alchemicalWizardry.api.spell.APISpellHelper;
+
+
+public class SpellHelper
+{
+
+    public static Vec3 createVec3(double x, double y, double z)
+    {
+    	return APISpellHelper.createVec3(x, y, z);
+    }
+    
+}
