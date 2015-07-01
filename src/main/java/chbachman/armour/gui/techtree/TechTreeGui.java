f6e609a78bdd2f866e8502d3777851c27bdeb5e5
chbachman <cbachman@bachmangroup.com>
2015-07-01 13:55:30 -0500

    Start of Gui

---
+++ src/main/java/chbachman/armour/gui/techtree/TechTreeGui.java
@@ -0,0 +1,20 @@
+package chbachman.armour.gui.techtree;
+
+import net.minecraft.inventory.Container;
+import cofh.core.gui.GuiBaseAdv;
+
+public class TechTreeGui extends GuiBaseAdv{
+
+	public TechTreeGui(Container arg0) {
+		super(arg0);
+	}
+	
+	public void drawForeground(){
+		
+	}
+	
+	public void drawBackground(){
+		
+	}
+
+}
